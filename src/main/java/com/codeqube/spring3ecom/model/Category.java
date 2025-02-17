b5cd1bfadeb960781e1cfc2fcbf2a0ca9a0bdee1
pragra <sunny@pragra.io>
2025-02-17 11:11:59 -0500

    Custom Error handler

--- src/main/java/com/codeqube/spring3ecom/model/Category.java
+++ src/main/java/com/codeqube/spring3ecom/model/Category.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity(name = "Cat")
@@ -14,6 +15,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
+    @NotBlank
     private String categoryName;
 
     public Long getCategoryId() {

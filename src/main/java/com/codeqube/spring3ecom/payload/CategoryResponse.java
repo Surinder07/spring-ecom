cd750868817376cdfbc2bba15519059806d941d7
pragra <sunny@pragra.io>
2025-02-17 20:55:31 -0500

    set meta data  for the pagination

--- src/main/java/com/codeqube/spring3ecom/payload/CategoryResponse.java
+++ src/main/java/com/codeqube/spring3ecom/payload/CategoryResponse.java
@@ -8,6 +8,11 @@ import java.util.List;
 @AllArgsConstructor
 public class CategoryResponse {
     List<CategoryDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private boolean lastPage;
 
     public List<CategoryDTO> getContent() {
         return content;
@@ -15,5 +20,45 @@ public class CategoryResponse {
 
     public void setContent(List<CategoryDTO> content) {
         this.content = content;
+    }
+
+    public Integer getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(Long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public boolean isLastPage() {
+        return lastPage;
+    }
+
+    public void setLastPage(boolean lastPage) {
+        this.lastPage = lastPage;
     }
 }
